603a3daa0ff65970c7828343b13e2ff8f12ab9b8
Siddharth-22-1994 <64464154+Siddharth-22-1994@users.noreply.github.com>
2024-03-11 15:57:20 +0530

    Add files via upload

---
+++ ksrm.py
@@ -0,0 +1,96 @@
+'''
+
+
+'''
+
+
+# print("hello world")
+
+# import this
+# print(this)
+
+# from file_1 import a, b
+# print(a)
+# print(b)
+
+# import antigravity
+# class arithmatic:
+#     def sum(self, a, b):
+#         print(a+b)
+        
+#     def sub(self, a, b):
+#         print(a-b)
+        
+# class scientific:
+#     def sin(self):
+#         print("its sin function")
+        
+# obj1 = arithmatic()
+# operation =input("Enter the operation: ")
+# if operation=="+":
+#     obj1.sum(2, 3)
+# elif operation =="-":
+#     obj1.sub(6,3)
+
+
+# a = 7
+# b = 8
+# print(a, b)
+# c = a+t
+# print(c)
+
+# ---> print(object, sep, end, file, flush)
+# print has 5 parameters
+
+# object  -->the value to be printed is object
+# sep--> seperator
+# Eg
+# a = 8
+# b = 9
+# c = 89
+# print(a, b,c,  sep='$')
+
+# end --> which is used to add the string at the end of the value, 
+# # It will not keep the command to stay in same line
+# Eg:
+# print(str(a), b, end='%%')
+# print(str(c))
+
+# ! ----> variables
+# ?variable is a name which is used to identify the value of variable
+# ?which is stored in a specific memory location
+
+# ! Rules to dedfine variables
+# 1.) Variable have to start with only alphabets, underscores
+# 2.) The char following the 1st char can be alphabets, numbers, underscores
+# 3.) No spaces are allowed
+# 4.) No special characters like !@#$%^&*()+
+# 5.) case sensitive
+# 6.) Keywords are not allowed as variables
+
+# ! ways to define variables
+
+# a = 8
+# b = 9
+# print(a, b)
+
+# a=7, b = 9 # error
+# print(a, b)
+
+# a, b = 7, 9
+# print(a)
+# print(b)
+
+
+# a=7
+# b=7
+
+# a = b = 7
+# print(a, b)
+
+# a=8, 7, 9.89, "hello"
+# print(a)
+# print(type(a))
+
+a, b = 78
+print(a, b)
